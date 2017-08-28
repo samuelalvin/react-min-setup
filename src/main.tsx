fa6eae55c91011e8e08cecd9d40b6ae5300e359f
samuelalvin <samuel.alvin1994@gmail.com>
2017-08-28 16:22:17 +0700

    add routing feature

--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,17 @@
 import * as React from "react";
 import * as ReactDOM from "react-dom";
+import { BrowserRouter, Switch, Route } from "react-router-dom";
 import App from "./app/app"
 
+const main = (
+    <BrowserRouter>
+        <Switch>
+            <Route exact path="/" component={App}></Route>
+        </Switch>
+    </BrowserRouter>
+);
+
 ReactDOM.render(
-    <App></App>,
-    document.getElementById("app")
+    main,
+    document.getElementById("main")
 );
